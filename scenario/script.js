7ba05d948d2240c30dfbfa22beeaf44fb2e4dddf
Gabriel Nascimento <gabriel.nascimento@construo.ch>
2022-12-30 22:16:14 +0000

    k6 html report

---
+++ scenario/script.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
+
+
+export default function () {
+  http.get('http://test.k6.io');
+}
